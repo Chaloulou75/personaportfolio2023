12cb4b4302516c58a6f3167929c43063e07920a0
Chaloulou75 <c.jeandey@gmail.com>
2023-02-10 13:55:17 +0100

    stores works 3 ok

--- stores/WorkStore.js
+++ stores/WorkStore.js
@@ -489,4 +489,477 @@ export let works = reactive([
     ],
     programs: ["Excel", "Word", "Illustrator"],
   },
+  {
+    name: "WeAr Denim",
+    slug: "wear-denim",
+    year: "2021",
+    categories: ["#Media & Journalism"],
+    background: "bg-dia-2 from-red-600 to-redjuli",
+    background2: "bg-gray-900",
+    description:
+      "I collaborate with reports, interviews and articles to highlight the latest fashion and art trends. WeAr global B2B magazine for fashion &amp; footwear is published in 8 language versions: German, English, French, Italian, Japanese, Mandarin, Russian and French. It is distributed to over 50 countries on all 5 continents. WeAr serves as inspiration for leading fashion buyers, influencers, trendsetters and decision makers in fashion, art and footwear globally.",
+    images: [
+      {
+        id: 1,
+        name: "uno",
+        href: "/img/proyectos/diados/1.jpg",
+      },
+      {
+        id: 2,
+        name: "dos",
+        href: "/img/proyectos/diados/2.jpg",
+      },
+      {
+        id: 3,
+        name: "tres",
+        href: "/img/proyectos/diados/3.jpg",
+      },
+      {
+        id: 4,
+        name: "cuatro",
+        href: "/img/proyectos/diados/4.jpg",
+      },
+      {
+        id: 5,
+        name: "cinco",
+        href: "/img/proyectos/diados/5.jpg",
+      },
+      {
+        id: 6,
+        name: "seis",
+        href: "/img/proyectos/diados/6.jpg",
+      },
+      {
+        id: 7,
+        name: "siete",
+        href: "/img/proyectos/diados/7.jpg",
+      },
+      {
+        id: 8,
+        name: "ocho",
+        href: "/img/proyectos/diados/8.jpg",
+      },
+      {
+        id: 9,
+        name: "nueve",
+        href: "/img/proyectos/diados/9.jpg",
+      },
+    ],
+    tookPartIn: ["Writing & reporting"],
+    programs: ["Excel", "Word"],
+  },
+  {
+    name: "WeAr Global Magazine",
+    slug: "wear-global-magazine",
+    year: "2021",
+    categories: ["#Media & Journalism"],
+    background: "bg-dia-1 from-black to-gray-800",
+    background2: "bg-gray-900",
+    description:
+      "I currently collaborate with reports, interviews and articles to highlight the latest denim trends. WeAr denim magazine covers sustainable technologies and important players in the international denim market. WeAr Denim reports to the Denim community in its entirety: sourcing, fabrics, yarns, washing, brands and the relevant retailers. It will communicate about the latest innovations, sustainable technologies and the important players and innovators.",
+    images: [
+      {
+        id: 1,
+        name: "uno",
+        href: "/img/proyectos/diauno/1.jpg",
+      },
+      {
+        id: 2,
+        name: "dos",
+        href: "/img/proyectos/diauno/2.jpg",
+      },
+      {
+        id: 3,
+        name: "tres",
+        href: "/img/proyectos/diauno/3.jpg",
+      },
+      {
+        id: 4,
+        name: "cuatro",
+        href: "/img/proyectos/diauno/4.jpg",
+      },
+      {
+        id: 5,
+        name: "cinco",
+        href: "/img/proyectos/diauno/5.jpg",
+      },
+      {
+        id: 6,
+        name: "seis",
+        href: "/img/proyectos/diauno/6.jpg",
+      },
+      {
+        id: 7,
+        name: "siete",
+        href: "/img/proyectos/diauno/7.jpg",
+      },
+      {
+        id: 8,
+        name: "ocho",
+        href: "/img/proyectos/diauno/8.jpg",
+      },
+      {
+        id: 9,
+        name: "nueve",
+        href: "/img/proyectos/diauno/9.jpg",
+      },
+      {
+        id: 10,
+        name: "dies",
+        href: "/img/proyectos/diauno/10.jpg",
+      },
+      {
+        id: 11,
+        name: "once",
+        href: "/img/proyectos/diauno/11.jpg",
+      },
+    ],
+    tookPartIn: ["Writing & reporting"],
+    programs: ["Excel", "Word"],
+  },
+  {
+    name: "Committee A.W.A.K.E",
+    slug: "committee-awake",
+    year: "2021",
+    categories: ["#Sustainability"],
+    background: "bg-dia-3 from-blue-500 to-bluejuli",
+    background2: "bg-blue-900",
+    description:
+      "This committee was founded during my time spent in Santista/Tavex to drive our collection to a more sustainable approach. We worked together with other areas, including the Workwear direction, Marketing, Investigation &amp; Development department, and TQC managers. We conceived a plan, and I led this strategy for our department to try to have an impact in every stage of denim production. Guided by the SDG (sustainable development goals) and the ISO 14001 (environmental management system).",
+    images: [
+      {
+        id: 1,
+        name: "uno",
+        href: "/img/proyectos/diatres/1.jpg",
+      },
+      {
+        id: 2,
+        name: "dos",
+        href: "/img/proyectos/diatres/2.jpg",
+      },
+      {
+        id: 3,
+        name: "tres",
+        href: "/img/proyectos/diatres/3.jpg",
+      },
+      {
+        id: 4,
+        name: "cuatro",
+        href: "/img/proyectos/diatres/4.jpg",
+      },
+      {
+        id: 5,
+        name: "cinco",
+        href: "/img/proyectos/diatres/5.jpg",
+      },
+      {
+        id: 6,
+        name: "seis",
+        href: "/img/proyectos/diatres/6.jpg",
+      },
+      {
+        id: 7,
+        name: "siete",
+        href: "/img/proyectos/diatres/7.jpg",
+      },
+      {
+        id: 8,
+        name: "ocho",
+        href: "/img/proyectos/diatres/8.jpg",
+      },
+      {
+        id: 9,
+        name: "nueve",
+        href: "/img/proyectos/diatres/9.jpg",
+      },
+      {
+        id: 10,
+        name: "dies",
+        href: "/img/proyectos/diatres/10.jpg",
+      },
+      {
+        id: 11,
+        name: "once",
+        href: "/img/proyectos/diatres/11.jpg",
+      },
+      {
+        id: 12,
+        name: "diesdos",
+        href: "/img/proyectos/diatres/12.jpg",
+      },
+      {
+        id: 13,
+        name: "diestres",
+        href: "/img/proyectos/diatres/13.jpg",
+      },
+      {
+        id: 14,
+        name: "diescuatro",
+        href: "/img/proyectos/diatres/14.jpg",
+      },
+      {
+        id: 15,
+        name: "diescinco",
+        href: "/img/proyectos/diatres/15.jpg",
+      },
+      {
+        id: 16,
+        name: "diesseis",
+        href: "/img/proyectos/diatres/16.jpg",
+      },
+      {
+        id: 17,
+        name: "diessiete",
+        href: "/img/proyectos/diatres/17.jpg",
+      },
+      {
+        id: 18,
+        name: "diesocho",
+        href: "/img/proyectos/diatres/18.jpg",
+      },
+      {
+        id: 19,
+        name: "diesnueve",
+        href: "/img/proyectos/diatres/19.jpg",
+      },
+      {
+        id: 20,
+        name: "veinte",
+        href: "/img/proyectos/diatres/20.jpg",
+      },
+    ],
+    tookPartIn: [
+      "Definition of the strategy",
+      "Alignments to the SDG (sustainable development goals, UN)",
+      "Coordination of the meetings",
+      "Follow-Up the actions and plans",
+    ],
+    programs: ["Google project", "Excel", "SDG tools"],
+  },
+  {
+    name: "DUO",
+    slug: "duo",
+    year: "2019",
+    categories: ["#Sustainability"],
+    background: "bg-dia-4 from-yellow-500 to-yellowjuli",
+    background2: "bg-yellowjuli",
+    description:
+      "Duo is a brand project in Buenos Aires made for the Gen Z target audience, who understand that the way we shop has to match with our ideals and values. As part of the BE DISOBEDIENT consultancy team, I coordinated the sustainable strategy for the brand. We worked with DUO’s team for two months. First, we incorporated principles about sustainability: Life cycle assessments and System thinking. Then, we focused on the collection, and together we developed changes in the supply chain that aligned with the main ideals of DUO.",
+    images: [
+      {
+        id: 1,
+        name: "uno",
+        href: "/img/proyectos/diacuatro/1.jpg",
+      },
+      {
+        id: 2,
+        name: "dos",
+        href: "/img/proyectos/diacuatro/2.jpg",
+      },
+      {
+        id: 3,
+        name: "tres",
+        href: "/img/proyectos/diacuatro/3.jpg",
+      },
+      {
+        id: 4,
+        name: "cuatro",
+        href: "/img/proyectos/diacuatro/4.jpg",
+      },
+      {
+        id: 5,
+        name: "cinco",
+        href: "/img/proyectos/diacuatro/5.jpg",
+      },
+      {
+        id: 6,
+        name: "seis",
+        href: "/img/proyectos/diacuatro/6.jpg",
+      },
+      {
+        id: 7,
+        name: "siete",
+        href: "/img/proyectos/diacuatro/7.jpg",
+      },
+    ],
+    tookPartIn: [
+      "Research potential suppliers",
+      "LCA design",
+      "Design for sustainability",
+      "Design for Ethical production",
+    ],
+    programs: ["Illustrator", "Keynote", "Excel"],
+  },
+  {
+    name: "Fibra",
+    slug: "fibra",
+    year: "2020",
+    categories: ["#Sustainability"],
+    background: "bg-dia-5 from-gray-900 to-black",
+    background2: "bg-gray-800",
+    description:
+      "FIBRA was a textile project in collaboration with Santista, EnRedando, Josefina Luna and local artisans from Argentina. EnRedando is a collaborative network that develops textile projects with a social and environmental impact. Josefina Luna is a designer who works with local communities and artisans to improve the Argentinian design identity. We worked together trying to find our identity in Denim, looking at the handcrafted textile work from Argentinean artisans, and local communities. We worked on a small scale to create a collection of fabrics, handwoven with ancient techniques by the artisans. We also explored the connection between the industry and local producers, and we found out that in Argentina we are still working in a local way, with natural fibres that grow on our land, even on a large scale. We developed three fabrics: TINKU, OHUANTA and RANDA, each of them made in a different region from our country, and handwoven with a different technique specific from each community. To learn more about the people involved in this project: ENREDANDO (https://www.grupoenredando.com/proyecto-fibra)",
+    images: [
+      {
+        id: 1,
+        name: "uno",
+        href: "/img/proyectos/diacinco/1.jpg",
+      },
+      {
+        id: 2,
+        name: "dos",
+        href: "/img/proyectos/diacinco/2.jpg",
+      },
+      {
+        id: 3,
+        name: "tres",
+        href: "/img/proyectos/diacinco/3.jpg",
+      },
+      {
+        id: 4,
+        name: "cuatro",
+        href: "/img/proyectos/diacinco/4.jpg",
+      },
+      {
+        id: 5,
+        name: "cinco",
+        href: "/img/proyectos/diacinco/5.jpg",
+      },
+      {
+        id: 6,
+        name: "seis",
+        href: "/img/proyectos/diacinco/6.jpg",
+      },
+      {
+        id: 7,
+        name: "siete",
+        href: "/img/proyectos/diacinco/7.jpg",
+      },
+      {
+        id: 8,
+        name: "ocho",
+        href: "/img/proyectos/diacinco/8.jpg",
+      },
+      {
+        id: 9,
+        name: "nueve",
+        href: "/img/proyectos/diacinco/9.jpg",
+      },
+      {
+        id: 10,
+        name: "dies",
+        href: "/img/proyectos/diacinco/10.jpg",
+      },
+    ],
+    tookPartIn: [
+      "Project coordination",
+      "Raw material supplier",
+      "Communication & lookbook",
+      "Video conference of the project",
+    ],
+    programs: ["Excel", "Google project"],
+  },
+  {
+    name: "Natural pigments",
+    slug: "natural-pigments",
+    year: "2018",
+    categories: ["#Sustainability"],
+    background: "bg-dia-6 from-blue-500 to-bluejuli",
+    background2: "bg-blue-800",
+    description:
+      "We were the first denim mill in Argentina to launch products dyed with natural pigments on a large scale. This was a part of the program research developed during my time spent in Santista as a Jeanswear Product Manager. This action was part of a sustainability strategy program that we developed in the “Committee A.W.A.K.E”. This committee was founded during my time spent in Santista/Tavex to drive our collection to a more sustainable approach.",
+    images: [
+      {
+        id: 1,
+        name: "uno",
+        href: "/img/proyectos/diaseis/1.jpg",
+      },
+      {
+        id: 2,
+        name: "dos",
+        href: "/img/proyectos/diaseis/2.jpg",
+      },
+      {
+        id: 3,
+        name: "tres",
+        href: "/img/proyectos/diaseis/3.jpg",
+      },
+      {
+        id: 4,
+        name: "cuatro",
+        href: "/img/proyectos/diaseis/4.jpg",
+      },
+      {
+        id: 5,
+        name: "cinco",
+        href: "/img/proyectos/diaseis/5.jpg",
+      },
+      {
+        id: 6,
+        name: "seis",
+        href: "/img/proyectos/diaseis/6.jpg",
+      },
+      {
+        id: 7,
+        name: "siete",
+        href: "/img/proyectos/diaseis/7.jpg",
+      },
+      {
+        id: 8,
+        name: "ocho",
+        href: "/img/proyectos/diaseis/8.jpg",
+      },
+      {
+        id: 9,
+        name: "nueve",
+        href: "/img/proyectos/diaseis/9.jpg",
+      },
+      {
+        id: 10,
+        name: "dies",
+        href: "/img/proyectos/diaseis/10.jpg",
+      },
+      {
+        id: 11,
+        name: "once",
+        href: "/img/proyectos/diaseis/11.jpg",
+      },
+      {
+        id: 12,
+        name: "diesdos",
+        href: "/img/proyectos/diaseis/12.jpg",
+      },
+      {
+        id: 13,
+        name: "diestres",
+        href: "/img/proyectos/diaseis/13.jpg",
+      },
+      {
+        id: 14,
+        name: "diescuatro",
+        href: "/img/proyectos/diaseis/14.jpg",
+      },
+      {
+        id: 15,
+        name: "diescinco",
+        href: "/img/proyectos/diaseis/15.jpg",
+      },
+      {
+        id: 16,
+        name: "diesseis",
+        href: "/img/proyectos/diaseis/16.jpg",
+      },
+      {
+        id: 17,
+        name: "diessiete",
+        href: "/img/proyectos/diaseis/17.jpg",
+      },
+    ],
+    tookPartIn: [
+      "Search for suppliers",
+      "Project coordination",
+      "Costing & Pricing",
+    ],
+    programs: ["Google project", "Excel"],
+  },
 ]);
